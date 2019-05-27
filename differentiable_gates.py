c6cc8c3d64ce810f02d5998bcf3572e22e40c351
Jamie <jamcc09@gmail.com>
2019-05-27 21:39:43 +0200

    Added cross entropy gate, seems to work with labels

--- differentiable_gates.py
+++ differentiable_gates.py
@@ -7,6 +7,24 @@ def sigmoid(x):
     return 1/(1 + math.exp(-x))
 
 
+def cross_entropy(x, y):
+    eps = 1e-8
+    return y * math.log(x+eps) + (1 - y) * math.log(1-x+eps)
+
+
+def zeros_initializer(shape=(1,)):
+    pass
+
+
+class Initializer(object):
+
+    def __init__(self, function):
+        self.initialize = function
+
+    def __call__(self, *args, **kwargs):
+        return self.initialize(*args, **kwargs)
+
+
 class Unit(object):
 
     def __init__(self, value, gradient):
@@ -83,6 +101,21 @@ class Sigmoid(Gate):
         self.unit_0.gradient += x * (1 - x) * self.utop.gradient
 
 
+class CrossEntropy(Gate):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def forward(self, input_1, label):
+        self.unit_0 = input_1
+        self.utop = Unit(cross_entropy(input_1.value, label), 0.0)
+        return self.utop
+
+    def backward(self, y):
+        z = self.unit_0.value + 1e-8
+        self.unit_0.gradient += ((y/z) - ((1-y)/(1-z))) * self.utop.gradient
+
+
 def main():
 
     # Create input units
@@ -91,6 +124,8 @@ def main():
     c = Unit(-3.0, 0.0)
     x = Unit(-1.0, 0.0)
     y = Unit(3.0, 0.0)
+
+    label = 1
 
     print(a)
     print(b)
@@ -108,47 +143,49 @@ def main():
     add_gate_1 = Add("(a*x + b*y) + c")
     sig_gate_0 = Sigmoid("sigmoid(z)")
 
+    xent_gate_0 = CrossEntropy("Xent(s, y)")
+
     ax = mult_gate_0.forward(a, x)
     by = mult_gate_1.forward(b, y)
     ax_by = add_gate_0.forward(ax, by)
     z = add_gate_1.forward(ax_by, c)
     s = sig_gate_0.forward(z)
-
-    # print(ax)
-    # print(by)
-    # print(ax_by)
-    # print(z)
-    # print(s)
-
-    s.gradient = 1.0
-    sig_gate_0.backward()
-    add_gate_1.backward()
-    add_gate_0.backward()
-    mult_gate_1.backward()
-    mult_gate_0.backward()
+    ce = xent_gate_0.forward(s, label)
 
     step_size = 0.01
 
-    a.value += step_size * a.gradient
-    b.value += step_size * b.gradient
-    c.value += step_size * c.gradient
-    x.value += step_size * x.gradient
-    y.value += step_size * y.gradient
+    for i in range(0, 1000):
 
-    print("")
-    print(a)
-    print(b)
-    print(c)
-    print(x)
-    print(y)
+        ce.gradient = 1.0
+        xent_gate_0.backward(label)
+        sig_gate_0.backward()
+        add_gate_1.backward()
+        add_gate_0.backward()
+        mult_gate_1.backward()
+        mult_gate_0.backward()
 
-    print(s)
-    ax = mult_gate_0.forward(a, x)
-    by = mult_gate_1.forward(b, y)
-    ax_by = add_gate_0.forward(ax, by)
-    z = add_gate_1.forward(ax_by, c)
-    s = sig_gate_0.forward(z)
-    print(s)
+        a.value += step_size * a.gradient
+        b.value += step_size * b.gradient
+        c.value += step_size * c.gradient
+        x.value += step_size * x.gradient
+        y.value += step_size * y.gradient
+
+        print("")
+        print(a)
+        print(b)
+        print(c)
+        print(x)
+        print(y)
+        print("")
+
+        print(s, ce)
+        ax = mult_gate_0.forward(a, x)
+        by = mult_gate_1.forward(b, y)
+        ax_by = add_gate_0.forward(ax, by)
+        z = add_gate_1.forward(ax_by, c)
+        s = sig_gate_0.forward(z)
+        ce = xent_gate_0.forward(s, label)
+        print(s, ce)
 
 
 if __name__ == "__main__":
